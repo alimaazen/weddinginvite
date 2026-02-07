0bb8642c516d77c10b28eea85785d70fc13e9aba
Ali Maazen <alimaazen@gmail.com>
2026-02-07 15:41:09 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react()].filter(Boolean),
+  base: "/weddinginvite",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
